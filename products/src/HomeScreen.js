44aa15b9534a2b78c17dd16b3e0944a60be052b2
dongdq <quangdong211@gmail.com>
2022-09-26 17:47:23 +0700

    update homescreen

--- products/src/HomeScreen.js
+++ products/src/HomeScreen.js
@@ -1,21 +1,179 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-const HomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-    </View>
-  );
+const pokemon = {
+  count: 1154,
+  next: 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20',
+  previous: null,
+  results: [
+    {
+      name: 'bulbasaur',
+      url: 'https://pokeapi.co/api/v2/pokemon/1/',
+    },
+    {
+      name: 'ivysaur',
+      url: 'https://pokeapi.co/api/v2/pokemon/2/',
+    },
+    {
+      name: 'venusaur',
+      url: 'https://pokeapi.co/api/v2/pokemon/3/',
+    },
+    {
+      name: 'charmander',
+      url: 'https://pokeapi.co/api/v2/pokemon/4/',
+    },
+    {
+      name: 'charmeleon',
+      url: 'https://pokeapi.co/api/v2/pokemon/5/',
+    },
+    {
+      name: 'charizard',
+      url: 'https://pokeapi.co/api/v2/pokemon/6/',
+    },
+    {
+      name: 'squirtle',
+      url: 'https://pokeapi.co/api/v2/pokemon/7/',
+    },
+    {
+      name: 'wartortle',
+      url: 'https://pokeapi.co/api/v2/pokemon/8/',
+    },
+    {
+      name: 'blastoise',
+      url: 'https://pokeapi.co/api/v2/pokemon/9/',
+    },
+    {
+      name: 'caterpie',
+      url: 'https://pokeapi.co/api/v2/pokemon/10/',
+    },
+    {
+      name: 'metapod',
+      url: 'https://pokeapi.co/api/v2/pokemon/11/',
+    },
+    {
+      name: 'butterfree',
+      url: 'https://pokeapi.co/api/v2/pokemon/12/',
+    },
+    {
+      name: 'weedle',
+      url: 'https://pokeapi.co/api/v2/pokemon/13/',
+    },
+    {
+      name: 'kakuna',
+      url: 'https://pokeapi.co/api/v2/pokemon/14/',
+    },
+    {
+      name: 'beedrill',
+      url: 'https://pokeapi.co/api/v2/pokemon/15/',
+    },
+    {
+      name: 'pidgey',
+      url: 'https://pokeapi.co/api/v2/pokemon/16/',
+    },
+    {
+      name: 'pidgeotto',
+      url: 'https://pokeapi.co/api/v2/pokemon/17/',
+    },
+    {
+      name: 'pidgeot',
+      url: 'https://pokeapi.co/api/v2/pokemon/18/',
+    },
+    {
+      name: 'rattata',
+      url: 'https://pokeapi.co/api/v2/pokemon/19/',
+    },
+    {
+      name: 'raticate',
+      url: 'https://pokeapi.co/api/v2/pokemon/20/',
+    },
+  ],
 };
+
+function HomeScreen({username}) {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? 'gray' : 'white',
+  };
+
+  const zeroPad = (num, places) => String(num).padStart(places, '0');
+
+  const renderItem = ({item, index}) => {
+    return (
+      <View
+        style={{
+          height: 100,
+          flex: 1,
+          padding: 10,
+          margin: 10,
+          borderRadius: 5,
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderColor: 'black',
+          backgroundColor: '#EEE',
+        }}>
+        <Image
+          source={{
+            uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${zeroPad(
+              index + 1,
+              3,
+            )}.png`,
+          }}
+          style={{width: 80, height: 80}}
+        />
+        <Text style={{marginVertical: 10}}>{item.name}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={backgroundStyle}>
+        {!!username && <Text>Hi, {username}</Text>}
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: isDarkMode ? 'black' : 'white',
+          }}>
+          <FlatList
+            data={pokemon.results}
+            numColumns={4}
+            renderItem={renderItem}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
   },
 });
