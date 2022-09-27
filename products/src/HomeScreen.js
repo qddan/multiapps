59748b12cc30e1d205e820b4dd8f23a097cf0f19
dongdq <quangdong211@gmail.com>
2022-09-27 17:23:14 +0700

    Update pokemon list

--- products/src/HomeScreen.js
+++ products/src/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   FlatList,
   Image,
@@ -10,96 +10,29 @@ import {
   View,
 } from 'react-native';
 
-const pokemon = {
-  count: 1154,
-  next: 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20',
-  previous: null,
-  results: [
-    {
-      name: 'bulbasaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/1/',
-    },
-    {
-      name: 'ivysaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/2/',
-    },
-    {
-      name: 'venusaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/3/',
-    },
-    {
-      name: 'charmander',
-      url: 'https://pokeapi.co/api/v2/pokemon/4/',
-    },
-    {
-      name: 'charmeleon',
-      url: 'https://pokeapi.co/api/v2/pokemon/5/',
-    },
-    {
-      name: 'charizard',
-      url: 'https://pokeapi.co/api/v2/pokemon/6/',
-    },
-    {
-      name: 'squirtle',
-      url: 'https://pokeapi.co/api/v2/pokemon/7/',
-    },
-    {
-      name: 'wartortle',
-      url: 'https://pokeapi.co/api/v2/pokemon/8/',
-    },
-    {
-      name: 'blastoise',
-      url: 'https://pokeapi.co/api/v2/pokemon/9/',
-    },
-    {
-      name: 'caterpie',
-      url: 'https://pokeapi.co/api/v2/pokemon/10/',
-    },
-    {
-      name: 'metapod',
-      url: 'https://pokeapi.co/api/v2/pokemon/11/',
-    },
-    {
-      name: 'butterfree',
-      url: 'https://pokeapi.co/api/v2/pokemon/12/',
-    },
-    {
-      name: 'weedle',
-      url: 'https://pokeapi.co/api/v2/pokemon/13/',
-    },
-    {
-      name: 'kakuna',
-      url: 'https://pokeapi.co/api/v2/pokemon/14/',
-    },
-    {
-      name: 'beedrill',
-      url: 'https://pokeapi.co/api/v2/pokemon/15/',
-    },
-    {
-      name: 'pidgey',
-      url: 'https://pokeapi.co/api/v2/pokemon/16/',
-    },
-    {
-      name: 'pidgeotto',
-      url: 'https://pokeapi.co/api/v2/pokemon/17/',
-    },
-    {
-      name: 'pidgeot',
-      url: 'https://pokeapi.co/api/v2/pokemon/18/',
-    },
-    {
-      name: 'rattata',
-      url: 'https://pokeapi.co/api/v2/pokemon/19/',
-    },
-    {
-      name: 'raticate',
-      url: 'https://pokeapi.co/api/v2/pokemon/20/',
-    },
-  ],
-};
-
 function HomeScreen({username}) {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const [pokemon, setPokemon] = useState([]);
+  const [nextPokemonUrl, setNextPokemonUrl] = useState('');
+
+  const getPokemon = async url => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setNextPokemonUrl(data.next);
+    setPokemon(prevState => {
+      return [...prevState, ...data?.results];
+    });
+  };
+
+  const onloadMore = () => {
+    if (nextPokemonUrl) getPokemon(nextPokemonUrl);
+  };
+
+  useEffect(() => {
+    getPokemon('https://pokeapi.co/api/v2/pokemon/?limit=30');
+  }, []);
 
   const backgroundStyle = {
     flex: 1,
@@ -109,29 +42,14 @@ function HomeScreen({username}) {
   const zeroPad = (num, places) => String(num).padStart(places, '0');
 
   const renderItem = ({item, index}) => {
+    const imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${zeroPad(
+      index + 1,
+      3,
+    )}.png`;
     return (
-      <View
-        style={{
-          height: 100,
-          flex: 1,
-          padding: 10,
-          margin: 10,
-          borderRadius: 5,
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderColor: 'black',
-          backgroundColor: '#EEE',
-        }}>
-        <Image
-          source={{
-            uri: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${zeroPad(
-              index + 1,
-              3,
-            )}.png`,
-          }}
-          style={{width: 80, height: 80}}
-        />
-        <Text style={{marginVertical: 10}}>{item.name}</Text>
+      <View style={styles.item}>
+        <Image source={{uri: imageUrl}} style={{width: 80, height: 80}} />
+        <Text style={{margin: 10}}>{item.name}</Text>
       </View>
     );
   };
@@ -140,16 +58,21 @@ function HomeScreen({username}) {
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={backgroundStyle}>
-        {!!username && <Text>Hi, {username}</Text>}
         <View
           style={{
             flex: 1,
             backgroundColor: isDarkMode ? 'black' : 'white',
           }}>
           <FlatList
-            data={pokemon.results}
-            numColumns={1}
+            ListHeaderComponent={
+              !!username && <Text style={styles.hiLabel}>Hi, {username}</Text>
+            }
+            data={pokemon}
+            numColumns={3}
+            onEndReachedThreshold={0.7}
+            onEndReached={onloadMore}
             renderItem={renderItem}
+            keyExtractor={(item, idx) => `${idx}`}
           />
         </View>
       </View>
@@ -160,20 +83,21 @@ function HomeScreen({username}) {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
+  hiLabel: {
+    color: 'black',
+    fontSize: 28,
     fontWeight: '600',
+    margin: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  item: {
+    height: 100,
+    flex: 1,
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: 'black',
+    backgroundColor: '#EEE',
   },
 });
